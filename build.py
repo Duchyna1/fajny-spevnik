d78b747afc717cee084601d70c54fb442d7ce82d
MartinBellus <martin.bellus.4@gmail.com>
2021-08-05 17:35:54 +0200

    python-build script

---
+++ build.py
@@ -0,0 +1,27 @@
+import os
+
+other = ".\Ostatné"
+
+main = open("./main.tex","w",encoding="UTF-16")
+h = open("./header.txt","r",encoding = "UTF-8")
+_h = h.readlines()
+h.close()
+main.writelines(_h)
+main.write("\n")
+rootDir = '.'
+exclude = [".git"]
+for dirName, subdirList, fileList in os.walk(rootDir):
+    print(dirName)
+    subdirList[:] = [d for d in subdirList if d not in exclude]
+    if dirName != "." and dirName != "./.git":
+        main.write("\\section{" + dirName[3:] + "}\n")
+        if dirName == other:
+            main.write("\\setleadsheets{title-template = other}\n")
+        for song in fileList:
+            main.write("\t\\input{" + dirName[2:]+"/"+song + "}\n")
+        main.write("\n")
+main.write("\\end{multicols}\n")
+main.write("\\end{document}\n")
+main.close()
+
+a = input()
