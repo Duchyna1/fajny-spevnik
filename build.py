e6f92156614f4128802b717fb0e00aabb57f1b46
MartinBellus <martin.bellus.4@gmail.com>
2021-08-07 13:38:50 +0200

    added deja vu and anime folder

--- build.py
+++ build.py
@@ -1,9 +1,8 @@
 import os
 
-other = ".\\3Ostatné"
+other = ["Ostatné","Anime"]
 
 main = open("./main.tex","w",encoding="UTF-8")
-main.write("\n")
 h = open("./header.txt","r",encoding = "UTF-8")
 _h = h.readlines()
 h.close()
@@ -15,8 +14,10 @@ for dirName, subdirList, fileList in os.walk(rootDir):
     print(dirName)
     subdirList[:] = sorted([d for d in subdirList if d not in exclude])
     if dirName != "." and dirName != "./.git":
-        if dirName == other:
+        if dirName[3:] in other:
             main.write("\\setleadsheets{title-template = other}\n")
+        else:
+            main.write("\\setleadsheets{title-template = custom}\n")
         main.write("\\section{" + dirName[3:] + "}\n")
         for song in sorted(fileList):
             if ".tex" in song: main.write("\t\\input{./" + dirName[2:]+"/"+song + "}\n")
